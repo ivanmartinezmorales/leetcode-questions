8fbf56c6cd9439c152b1722928e0d34b25d41ea0
Ivan Martinez Morales <35318080+ivanmartinezmorales@users.noreply.github.com>
2020-09-07 14:02:52 -0700

    Create reverseString.java

---
+++ reverseString.java
@@ -0,0 +1,18 @@
+class reverseString {
+    public static void main(String[] args) {
+      int case1 = 123;
+      int expected = reverse(case1);
+      System.out.println(case1);
+      System.out.println(expected);
+    }
+
+    public int reverse(int x) {
+        // 123
+        String reversed = new StringBuilder().append(Math.abs(x)).reverse().toString(); // 321
+        try {
+            return (x < 0) ? Integer.parseInt(reversed) * -1 : Integer.parseInt(reversed);
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+}
