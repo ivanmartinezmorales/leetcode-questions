2fa166bcf6deb3c8dc7c15881c028ebd3764494a
Ivan Martinez Morales <35318080+ivanmartinezmorales@users.noreply.github.com>
2020-09-07 20:11:20 -0700

    Create MaxSubArray.java

---
+++ MaxSubArray.java
@@ -0,0 +1,36 @@
+public class MaxSubArray {
+
+    public static void main(String[] args) {
+      int[] nums = [-2,1,-3,4,-1,2,1,-5,4];
+      int solution = maxSubArray(nums);
+      
+      if (solution == 6) {
+        System.out.println("You did it, it's right!");
+      }
+      
+    }
+    
+    public int maxSubArray(int[] nums) {
+        // This is a good candidate for a greedy algorithmic approach
+        // Basically in this approach, we're going to take an algorithm
+        // that basically picks a locally optimal move at each step
+        // first we need the value of the current element, and then we need the value
+        // of the local maximum element, and then we need the value of the global maximum that
+        // we've **seen so far**.
+        
+        // So first:
+        // we have two integers, one to keep track of the current sum, and one to keep track of the maximum sum
+        int currentSum = nums[0]; // which will be set to our first element since that first element in a possible 
+        // subarrray
+        int maxSum = nums[0]; // is the largest sum we've seen so far. This is our global maximum
+        // Now lets iterate over the entire list, omitting the first element since we've already accounted for it
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]); // finding our largest local solution so far
+            maxSum = Math.max(maxSum, currentSum); // finding our global maximum solution
+        }
+        // when we're done iterating through the entire list, we've found our biggest number in maxSum
+        
+        return maxSum;
+        
+    }
+}
