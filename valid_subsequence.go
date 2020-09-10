00086f1f5cc51f3ffde66e2742725ea43f1817f2
Ivan Martinez Morales <ivan.martinez.morales@gmail.com>
2020-09-09 21:47:25 -0700

    Add more go

---
+++ valid_subsequence.go
@@ -0,0 +1,13 @@
+package main
+
+func IsValidSubsequence(array []int, sequence []int) bool {
+	arrayIndex := 0
+	sequenceIndex := 0
+	for arrayIndex < len(array) && sequenceIndex < len(sequence) {
+		if array[arrayIndex] == sequence[sequenceIndex] {
+			sequenceIndex += 1
+		}
+		arrayIndex += 1
+	}
+	return sequenceIndex == len(sequence)
+}
