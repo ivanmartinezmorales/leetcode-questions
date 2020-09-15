765beafe6293772d16f60ffc6ce2b8d25512bea0
Ivan Martinez Morales <ivan.martinez.morales@gmail.com>
2020-09-14 20:49:28 -0700

    Added BFS

--- java/BFS.java
+++ java/BFS.java
@@ -1,3 +1,28 @@
+import java.util.*;
+
 public class BFS {
-		
+    static class Node {
+       String name;
+        List<Node> children = new ArrayList<Node>();
+        public Node(String name) {
+            this.name = name;
+        }
+   
+    public List<String> breadthFirstSearch(List<String> arr) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(this);
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            arr.add(current.name);
+            queue.addAll(current.children);
+        }
+        return arr;
+    }
+
+    public Node addChild(String name) {
+        Node child = new Node(name);
+        children.add(child);
+        return this;
+    }
+    }
 }
