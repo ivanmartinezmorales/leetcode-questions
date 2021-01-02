1dccfd627b57052d5fdf9fae8c66c29e01a264ec
Ivan Martinez Morales <ivan.martinez.morales@gmail.com>
2021-01-01 20:25:01 -0700

    Added single_cycle.go

---
+++ golang/single_cycle.go
@@ -0,0 +1,31 @@
+package main
+
+func HasSingleCycle(array []int) bool {
+	return hasSingleCycle(array, 0)
+}
+
+func hasSingleCycle(array []int, startingIndex int) bool {
+	// O(n) time | O(1) space
+	elementsVisited := 0
+	current := startingIndex
+
+	for elementsVisited < len(array) {
+		if elementsVisited > 0 && current == startingIndex {
+			return false
+		}
+		elementsVisited++
+		current = getNextIdx(current, array)
+	}
+
+	return current == startingIndex
+}
+
+func getNextIdx(current int, array []int) int {
+	jump := array[current]
+
+	if nextIdx := (current + jump) % len(array); nextIdx >= 0 {
+		return nextIdx
+	} else {
+		return nextIdx + len(array)
+	}
+}
