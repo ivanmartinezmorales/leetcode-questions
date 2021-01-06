c02c5341d6072cbf470adf765ef62cdd4b4e13a0
Ivan Martinez Morales <ivan.martinez.morales@gmail.com>
2021-01-05 20:50:51 -0700

    leetcode a day keeps the unemployment away

---
+++ golang/delete_duplicates.go
@@ -0,0 +1,26 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// DeleteDuplicates removes duplicated members from a sorted linked list
+// and returns the sorted list in order.
+func DeleteDuplicates(head *ListNode) *ListNode {
+	s := &ListNode{Next: head}
+	p := s
+
+	for head != nil && head.Next != nil {
+		if head.Val == head.Next.Val {
+			for head.Next != nil && head.Val == head.Next.Val {
+				head.Next = head.Next.Next
+			}
+			p.Next = head.Next
+		} else {
+			p = head
+		}
+		head = head.Next
+	}
+	return s.Next
+}
