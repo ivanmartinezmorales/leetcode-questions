1ed16102a32db89e233ffa5512cafca7e9b58f35
Ivan Martinez Morales <ivan.martinez.morales@gmail.com>
2021-01-07 22:01:58 -0700

    Daily yeetcode

---
+++ golang/missing_positive_number.go
@@ -0,0 +1,22 @@
+package main
+
+func findKthPositive(arr []int, k int) int {
+	if k <= arr[0]-1 {
+		return k
+	}
+
+	k -= arr[0] - 1
+
+	n := len(arr)
+
+	for i := 0; i < n-1; i++ {
+		c := arr[i+1] - arr[i] - 1
+		if k <= c {
+			return arr[i] + k
+		}
+
+		k -= c
+	}
+
+	return arr[n-1] + k
+}
