cbcaeb1e0c9518095e9a3b3ce14ccf5246c586e0
Ivan Martinez Morales <ivan.martinez.morales@gmail.com>
2021-01-11 16:05:40 -0700

    Add more utils

--- golang/utils/utils.go
+++ golang/utils/utils.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"errors"
 	"math"
+	"reflect"
 )
 
 // Contains returns true if the slice contains the value
@@ -60,4 +62,29 @@ func MinInt(a ...int) int {
 		}
 		return m
 	}
+}
+
+// InsertAtSlice inserts an element to a slice in the specified index
+func InsertAtSlice(slice interface{}, index int, value interface{}) (interface{}, error) {
+	v := reflect.ValueOf(slice)
+
+	if v.Kind() != reflect.Slice {
+		return nil, errors.New("this is not a slice")
+	}
+
+	if index < 0 || index > v.Len() || reflect.TypeOf(slice).Elem() != reflect.ValueOf(value) {
+		return nil, errors.New("param is invalid")
+	}
+
+	t := reflect.MakeSlice(reflect.TypeOf(slice), 0, 0)
+	if index == v.Len() {
+		t = reflect.AppendSlice(t, v.Slice(0, v.Len()))
+		t = reflect.Append(t, reflect.ValueOf(value))
+		return t.Interface(), nil
+	}
+
+	t = reflect.AppendSlice(t, v.Slice(0, v.Len()))
+	t = reflect.AppendSlice(t, v.Slice(index, v.Len()))
+	t.Index(index).Set(reflect.ValueOf(value))
+	return t.Interface(), nil
 }
