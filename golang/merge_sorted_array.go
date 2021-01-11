95222b4a1b31ad6c1ace85cdfdae1a7bf34f7b78
Ivan Martinez Morales <ivan.martinez.morales@gmail.com>
2021-01-11 14:54:44 -0700

    Add reverse linked list and start merge sorted array

---
+++ golang/merge_sorted_array.go
@@ -0,0 +1,5 @@
+package main
+
+func merge(n1 []int, m int, n2 []int, n int) {
+
+}
---
+++ golang/reverse_linked_list.go
@@ -0,0 +1,21 @@
+package main
+
+type SinglyLinkedList struct {
+	Value int
+	Next  *SinglyLinkedList
+}
+
+// ReverseLinkedList takes the head of a SinglyLinkedList and reverses it
+// in place. Returns the new head.
+func ReverseLinkedList(head *SinglyLinkedList) *SinglyLinkedList {
+	var prev *SinglyLinkedList
+	cur := head
+	for cur != nil {
+		n := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = n
+	}
+
+	return prev
+}
