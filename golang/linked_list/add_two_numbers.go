311adbbdca48377507988d4458e6d40599071143
Ivan Martinez Morales <ivan.martinez.morales@gmail.com>
2021-01-15 21:24:14 -0700

    Update with new question

---
+++ golang/linked_list/add_two_numbers.go
@@ -0,0 +1,10 @@
+package lists
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func AddTwoNumbers() *ListNode {
+	return nil
+}
---
+++ java/AddTwoNumbers.java
@@ -0,0 +1,14 @@
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(listNode l1, ListNode l2) {
+        ListNode temp = new ListNode(0);
+        ListNode a = l1, 
+    }
+}
+
+public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
