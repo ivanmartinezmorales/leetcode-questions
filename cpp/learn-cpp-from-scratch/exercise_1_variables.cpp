769643210012732f6c751933e102f36e32eddc2b
Ivan Martinez Morales <ivan.martinez.morales@gmail.com>
2021-07-19 22:05:20 -0400

    added cpp stuff

---
+++ cpp/learn-cpp-from-scratch/exercise_1_variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+    cout << "Value of integer is" << 30 << endl;
+    cout << "Value of float is" << 30.78 << endl;
+    cout << "Value of integer is" << 45.1234 << endl;
+    cout << "Value of integer is" << 30 << endl;
+    cout << "Value of integer is" << 30 << endl;
+    cout << "Value of integer is" << 30 << endl;
+    return 0;
+}
---
+++ cpp/learn-cpp-from-scratch/introduction_exercise.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+void print(int n) {
+    cout << "The movie quote is: My name is Bond. James Bond!" << endl;
+}
+
+int main() {
+    print(0);
+    return 0;    
+}
